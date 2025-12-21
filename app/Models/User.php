f5d96c9bca7855e760d469feb167fbd111bb7d52
khaled <khashfehkh@gmail.com>
2025-12-21 11:32:51 +0300

    Add JWT auth (tymon/jwt-auth) and jwt config

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, JWTSubject;
 
     protected $fillable = [
         'first_name',
@@ -74,4 +75,16 @@ class User extends Authenticatable
     {
         return $this->hasOne(Secretary::class);
     }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    
 }
